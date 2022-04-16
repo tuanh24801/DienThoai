81c64316c0f9401a58d753ba8b2a8c8576ae9901
tuanh24801 <tuanh12a12001@gmail.com>
2022-04-16 21:28:56 +0700

    update giohang

--- process/themgiohang.php
+++ process/themgiohang.php
@@ -2,30 +2,30 @@
     session_start();
     require '../core/connection.php';
     $id_nguoidung = $_SESSION['nguoidung'];
-    if(isset($_GET['id'])){
-        $id_sanpham =  $_GET['id'];
+    if(isset($_POST['btndatmua'])){
+        $id_sanpham =  $_POST['id'];
+        $soluong = $_POST['soluong'];
     }
     $sql_check = "SELECT * FROM giohang WHERE id_nguoidung = '$id_nguoidung'";
     $result_check = mysqli_query($conn,$sql_check);
-    $row_check = mysqli_fetch_assoc($result_check);
-    if($row_check['id_sanpham'] == $id_sanpham){
-        $sql_update_sp = "UPDATE giohang SET soluong = (soluong + 1) WHERE id_nguoidung = '$id_nguoidung' AND id_sanpham = '$id_sanpham'";
-        $result_update_sp = mysqli_query($conn,$sql_update_sp);
-        if($result_update_sp){
-            echo 'cập nhật số lượng thành công';
-        }else{
-            echo 'cập nhật thất bại';
+    while( $row_check = mysqli_fetch_assoc($result_check)){
+        if($row_check['id_sanpham'] == $id_sanpham){
+            $sql_update_sp = "UPDATE giohang SET soluong = soluong + '$soluong' WHERE id_nguoidung = '$id_nguoidung' AND id_sanpham = '$id_sanpham'";
+            $result_update_sp = mysqli_query($conn,$sql_update_sp);
+            if($result_update_sp){
+                header("Location: ../view/giohang.php");
+            }
+            die();
         }
     }
-    else{
-        $sql = "INSERT INTO giohang(id_nguoidung, id_sanpham, soluong) VALUES ('$id_nguoidung', '$id_sanpham' , '1')";
-        $result = mysqli_query($conn,$sql);
-        if($result){
-           header("Location: ../view/giohang.php");
-        }else{
-            echo 'thêm vào giỏ hàng thất bại';
-        }
+    $sql = "INSERT INTO giohang(id_nguoidung, id_sanpham, soluong) VALUES ('$id_nguoidung', '$id_sanpham' , '1')";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+        header("Location: ../view/giohang.php");
+    }else{
+        echo 'thêm vào giỏ hàng thất bại';
     }
+    
     
     
 ?>
--- view/chitietsanpham.php
+++ view/chitietsanpham.php
@@ -8,32 +8,40 @@
     $row = mysqli_fetch_assoc($result);
 ?>
     <div class="container">
-        <div class="row p-5">
-            <div class="col-5 me-5">
-                <img src="./IMAGES/sanpham/<?php echo $row['anhsanpham']?>" alt="" style="width: 100%;">
-            </div>
-            <div class="col-6">
-                <div class="row">
-                    <h1><?php echo $row['ten']?></h1>
-                    <h2><?php echo $row['gia']?>vnđ</h2>
+        
+            <div class="row p-5">
+                <div class="col-5 me-5">
+                    <img src="./IMAGES/sanpham/<?php echo $row['anhsanpham']?>" alt="" style="width: 100%;">
                 </div>
-                <div class="row mt-5">
-                    <div class="col-4">
-                        <?php 
-                            if(isset($_SESSION['nguoidung'])){
-                                ?>
-                                    <a href="../process/themgiohang.php?id=<?php echo $id; ?>" class="btn btn-success">đặt mua</a>
-                                <?php
-                            }else{
-                                ?>
-                                    <a href="dangnhap.php" class="btn btn-success">đặt mua</a>
-                                <?php
-                            }
-                        ?>
-
+                <div class="col-6">
+                    <div class="row">
+                        <h1><?php echo $row['ten']?></h1>
+                        <h2><?php echo $row['gia']?>vnđ</h2>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">số lượng</label>
+                            <form action="../process/themgiohang.php" method="post">
+                            <input type="number" min="1" step="1" class="form-control" name = "soluong" id="exampleFormControlInput1" value ="1">
+                            <input type="hidden" value="<?php echo $id; ?>" name="id">
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-4">
+                            <?php 
+                                if(isset($_SESSION['nguoidung'])){
+                                    ?>
+                                        <input type="submit" value="đặt mua" class="btn btn-success" name="btndatmua">
+                                    <?php
+                                }else{
+                                    ?>
+                                        <a href="dangnhap.php" class="btn btn-success">đặt mua</a>
+                                    <?php
+                                }
+                            ?>
+                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        
     </div>
 <?php require './partice/footer.php';?>
